59c02d14af1f3de4823f3e08ff5c738be08bb37d
viktor <v.xhelilaj@gmail.com>
2016-08-16 19:32:06 -0400

    Simon shut up

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,43 @@
+def echo(repeat)
+  "#{repeat}"
+end
+
+def shout(shout)
+  "#{shout.upcase}"
+end
+
+def repeat(again)
+  "#{again} #{again}"
+end
+
+def repeat(word, num = 2)
+  var = "#{word} " * num
+  var.rstrip
+end
+
+def start_of_word(blah, num)
+  blah[0..num-1]
+end
+
+def first_word(string)
+  array = string.split
+  array[0]
+end
+
+def titleize(string)
+  words = string.split
+  words.each do |word|
+    if word.include?("and")
+      word.downcase!
+    elsif word.include?("the")
+      word.downcase!
+    elsif word.include?("over")
+    elsif word.length
+      word.capitalize!
+    end
+  end
+  words[0] = words[0].capitalize
+  title = words.join(" ")
+  title.rstrip
+  return title
+end
